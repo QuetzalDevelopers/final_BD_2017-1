430f53310460a382dfe812750badd32cc0134b9d
ManuNovas <enam_rda@yahoo.com.mx>
2016-12-02 00:13:52 -0600

    Archivo sql con los usuarios de la base

---
+++ usuarios.sql
@@ -0,0 +1,27 @@
+--Autores:
+--Quezada Ferreira Fernando
+--Novas Santamaría José Manuel
+
+connect sys/system as sysdba
+
+create user qn_proy_admin
+identified by admin123
+quota unlimited on users;
+
+create user qn_proy_user
+identified by user123;
+
+grant
+  create session,
+  create table,
+  create sequence,
+  create /*public*/ synonym,
+  create view,
+  create trigger,
+  create procedure 
+to qn_proy_admin;
+
+grant create session
+to qn_proy_user;
+
+--commit;
