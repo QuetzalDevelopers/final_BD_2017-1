782080a69e22a64c2d8c981d9cce2091f0a29004
ManuNovas <enam_rda@yahoo.com.mx>
2016-12-07 23:20:11 -0600

    Agregadas algunas secuencias

---
+++ secuencias.sql
@@ -0,0 +1,53 @@
+--Autores:
+--Quezada Ferreira Fernando
+--Novas Santamaría José Manuel
+
+connect qn_proy_admin/admin123
+
+create sequence seq_publicacion
+  start with 1
+  increment by 1;
+
+create sequence seq_suscriptor
+  start with 1
+  increment by 1;
+
+create sequence seq_peticion
+  start with 1
+  increment by 1;
+
+create sequence seq_autor
+  start with 1
+  increment by 1;
+
+create sequence seq_area_interes
+  start with 1
+  increment by 1;
+
+create sequence seq_estado_articulo
+  start with 1
+  increment by 1;
+
+create sequence seq_empleado
+  start with 1
+  increment by 1;
+
+create sequence seq_articulo
+  start with 1
+  increment by 1;
+
+create sequence seq_autor_articulo
+  start with 1
+  increment by 1;
+
+create sequence seq_historial_estado_articulo
+  start with 1
+  increment by 1;
+
+create sequence seq_publicacion_articulo
+  start with 1
+  increment by 1;
+
+create sequence seq_revisor_area
+  start with 1
+  increment by 1;
