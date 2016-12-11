e6d9d5c9f7164b59cca78d30261c25eed2557125
ManuNovas <enam_rda@yahoo.com.mx>
2016-12-10 18:20:57 -0600

    Agregada mi propuesta de sinónimos

---
+++ sinonimos.sql
@@ -0,0 +1,11 @@
+--Autores:
+--Quezada Ferreira Fernando
+--Novas Santamaría José Manuel
+
+connect qn_proy_admin/admin123
+
+create or replace public synonym PUBLI for publicacion;
+
+create or replace public synonym ART for articulo;
+
+create or replace public synonym PORTABLE for pdf;
