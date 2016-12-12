86297528b1f3116b702b20f6cfed51be575e8fa8
FernandoQF <fernando071195@gmail.com>
2016-12-12 16:13:51 -0600

    Create main.sql

---
+++ main.sql
@@ -0,0 +1,12 @@
+
+
+
+--conexión como sysdba
+connect sys/system as sysdba
+
+start entidades.sql
+start indices.sql
+start secuencias.sql
+start sinonimos.sql
+start triggers.sql
+start vistas.sql
