46f1ec26eca29e3f93d86904bf8e3a8f7a6cbdeb
ManuNovas <enam_rda@yahoo.com.mx>
2016-12-12 19:51:31 -0600

    Agregado el archivo main

---
+++ main.sql
@@ -0,0 +1,16 @@
+--Autores:
+--Quezada Ferreira Fernando
+--Novas Santamaría José Manuel
+
+connect qn_proy_admin/admin123
+
+@@entidades.sql
+@@secuencias.sql
+@@indices.sql
+@@sinonimos.sql
+@@vistas.sql
+@@triggers.sql
+@@procedimientos.sql
+@@carga_inicial.sql
+
+--commit;
